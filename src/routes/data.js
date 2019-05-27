a5a8ebb1ef9ae8d8ee62691745ed3f019b9f976f
Ruslan Lutfullin <jeudesprits@Ruslans-MacBook-Pro.local>
2019-05-28 00:34:45 +0500

    😤

--- src/routes/data.js
+++ src/routes/data.js
@@ -3,7 +3,7 @@ import { igFollowers } from '../actions';
 
 const router = Router();
 
-router.get('/data', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const { ig } = req.query;
   return res.json(await igFollowers.getListOfIGFollowersCounts(ig));
 });
