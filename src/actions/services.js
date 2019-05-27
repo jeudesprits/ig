fd1a4a6692a60e2ce943acc29716e0ed00521f14
Ruslan Lutfullin <jeudesprits@Ruslans-MacBook-Pro.local>
2019-05-27 22:47:37 +0500

    😬

--- src/actions/services.js
+++ src/actions/services.js
@@ -1,26 +1,22 @@
-import { promisify } from 'util';
-// eslint-disable-next-line camelcase
-import child_process from 'child_process';
+import { execSync } from 'child_process';
 import process from 'process';
 import { logger } from '../util';
 
-const exec = promisify(child_process.exec);
-
-async function convertVideoToBoxWithBlur() {
+function convertVideoToBoxWithBlur() {
   try {
-    await exec(
+    execSync(
       'ffmpeg -y -i "tmp/input.mp4" -vf scale=1080:1080 "tmp/container.mp4"'
     );
-    await exec(
+    execSync(
       'ffmpeg -y -i "tmp/container.mp4" -vf gblur=sigma=20 "tmp/blurred.mp4"'
     );
-    await exec(
+    execSync(
       `ffmpeg -y -i "tmp/input.mp4" -filter_complex "[0:v]scale='if(lt(in_h,in_w),1080,-1)':'if(lt(in_w,in_h),1080,-1)'" "tmp/content.mp4"`
     );
-    await exec(
+    execSync(
       'ffmpeg -y -i "tmp/container.mp4" -i "tmp/blurred.mp4" -filter_complex "[0:v][1:v]overlay=0:0" "tmp/mix.mp4"'
     );
-    await exec(
+    execSync(
       `ffmpeg -y -i "tmp/mix.mp4" -i "tmp/content.mp4" -filter_complex "[0:v][1:v]overlay='if(lt(h,w),0,(W-w)/2)':'if(lt(w,h),0,(H-h)/2)'" "tmp/output.mp4"`
     );
   } catch (error) {
@@ -28,9 +24,9 @@ async function convertVideoToBoxWithBlur() {
   }
 }
 
-async function uploadVideoToIG() {
+function uploadVideoToIG() {
   try {
-    await exec(
+    execSync(
       `php php_service/index.php --title ' ' --path '${process.cwd()}/tmp/output.mp4'`
     );
   } catch (error) {
