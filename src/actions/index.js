b399e50ed164fa5e8f8770f9aed2e42ea2cb4f68
Ruslan Lutfullin <jeudesprits@Ruslans-MacBook-Pro.local>
2019-05-27 22:38:43 +0500

    😬

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import igFollowers from './igFollowers';
 import tiktokVideos from './tiktokVideos';
+import services from './services';
 
-export { igFollowers, tiktokVideos };
+export { igFollowers, tiktokVideos, services };
